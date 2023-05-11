28e694dffbb0a37c15430fd3306bc180affd0b52
Matteo Fasulo <mat.fasulo@gmail.com>
2023-05-11 17:52:58 +0200

    python3 or python

--- start.sh
+++ start.sh
@@ -10,6 +10,13 @@ read -p "Enter your email: " EMAIL
 read -p "Enter your birthdate: " BIRTHDATE
 
 # Execute the main.py script with the provided arguments
-python main.py -m "$EMAIL" -b "$BIRTHDATE"
+if command -v python &> /dev/null; then
+    python main.py -m "$EMAIL" -b "$BIRTHDATE"
+# Fallback to python3 if python is not available
+elif command -v python3 &> /dev/null; then
+    python3 main.py -m "$EMAIL" -b "$BIRTHDATE"
+else
+    echo "Python interpreter not found. Please install Python or Python3."
+fi
 
 docker build -t hype_dashboard:latest .

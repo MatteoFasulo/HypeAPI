1299e4269b9d07129d1c4a19b2fad97c7acd0a75
Matteo Fasulo <mat.fasulo@gmail.com>
2023-05-13 16:10:32 +0200

    fix prog argument in argparse parser

--- src/hypebankapi/__main__.py
+++ src/hypebankapi/__main__.py
@@ -7,14 +7,14 @@ from .utils import save_json
 
 def main():
     parser = argparse.ArgumentParser(
-        prog="HypeAPI",
+        prog="HypeBankAPI",
         description='Fetch data from Hype API and create JSON folder with profile, balance, card and movements information'
     )
     parser.add_argument('-m', '--email', help='Email address', required=True, type=str)
     parser.add_argument('-b', '--birthdate', help='Birth date in ISO (YYYY-MM-DD)', required=True)
     parser.add_argument('-l', '--limit', help='Limit the number of transactions to fetch from Hype API. Default to 50', required=False, default=50, type=int)
     parser.add_argument('-v', '--verbose', action='store_true', required=False, default=False)
-    
+
     args = parser.parse_args()
 
     password = getpass('Hype password: ')
